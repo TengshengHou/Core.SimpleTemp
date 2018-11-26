8ae64b21dbeb4854eaf101899c086c72c64039c0
TengshengHou <95377774@qq.com>
2018-11-26 13:49:56 +0800

    首页抓取 Readme

--- Core.SimpleTemp.Mvc/Controllers/HomeController.cs
+++ Core.SimpleTemp.Mvc/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
@@ -27,13 +28,24 @@ namespace Core.SimpleTemp.Mvc.Controllers
         public async Task<IActionResult> IndexAsync()
         {
             ViewData["UserName"] = User.Identity.Name;
-            //var request = new HttpRequestMessage(HttpMethod.Get, "https://github.com/TengshengHou/Core.SimpleTemp/blob/master/README.md");
-            //request.Headers.Add("Accept", "application/vnd.github.v3+json");
-            //request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
-            //var client = _httpClientFactory.CreateClient();
-            //var response = await client.SendAsync(request);
-            //var str = await response.Content.ReadAsStringAsync();
-            
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, "https://github.com/TengshengHou/Core.SimpleTemp/blob/master/README.md");
+                request.Headers.Add("Accept", "application/vnd.github.v3+json");
+                request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+                var client = _httpClientFactory.CreateClient();
+                var response = await client.SendAsync(request);
+                var str = await response.Content.ReadAsStringAsync();
+
+                string pattern12 = "#我是抓取开始标记#(.*?)#我是抓取标记结束#";
+
+                var m = Regex.Match(str, pattern12, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+                ViewData["ReadmeHtml"] = m?.Value ?? "GitHub ReadMe抓取失败";
+            }
+            catch (Exception)
+            {
+
+            }
             return View("Index");
         }
     }
